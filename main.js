4e19c82ee377257b9f60825fceb79d012d5f8590
IngridJaimes <97192482+IngridJaimes@users.noreply.github.com>
2022-01-05 13:28:26 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.productos', {delay: 500});
+ScrollReveal().reveal('.Ubicación', {delay: 500});
+ScrollReveal().reveal('.banner', {delay: 500});
+ScrollReveal().reveal('.tabla', {delay: 500});
